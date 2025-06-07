0ac56d2bcd3e4c30257b74c6018f548f1a4ee2e3
Francisco Benegas <fbenegas@bancard.com.py>
2025-06-07 10:33:00 -0300

    Push init2

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,54 +1,54 @@
 import { NextResponse, NextRequest } from "next/server";
-import bcrypt from "bcrypt";
-import prisma from "../../../../libs/prisma";
+//import bcrypt from "bcrypt";
+//import prisma from "../../../../libs/prisma";
 
 export async function POST(request: NextRequest) {
   try {
     const data = await request.json();
+    return NextResponse.json(data);
+    // const emailFount = await prisma.usuario.findUnique({
+    //   where: {
+    //     email: data.email,
+    //   },
+    // });
 
-    const emailFount = await prisma.usuario.findUnique({
-      where: {
-        email: data.email,
-      },
-    });
+    // if (emailFount) {
+    //   return NextResponse.json(
+    //     {
+    //       message: "el Email ya existe",
+    //     },
+    //     { status: 400 }
+    //   );
+    // }
 
-    if (emailFount) {
-      return NextResponse.json(
-        {
-          message: "el Email ya existe",
-        },
-        { status: 400 }
-      );
-    }
+    // const userFount = await prisma.usuario.findUnique({
+    //   where: {
+    //     username: data.username,
+    //   },
+    // });
 
-    const userFount = await prisma.usuario.findUnique({
-      where: {
-        username: data.username,
-      },
-    });
+    // if (userFount) {
+    //   return NextResponse.json(
+    //     { message: "el Usuario ya existe" },
+    //     { status: 400 }
+    //   );
+    // }
 
-    if (userFount) {
-      return NextResponse.json(
-        { message: "el Usuario ya existe" },
-        { status: 400 }
-      );
-    }
+    // const hashPassw = await bcrypt.hashSync(data.password, 10);
 
-    const hashPassw = await bcrypt.hashSync(data.password, 10);
+    // const newUser = await prisma.usuario.create({
+    //   data: {
+    //     username: data.username,
+    //     email: data.email,
+    //     password: hashPassw,
+    //     establesimiento: data.establesimiento,
+    //   },
+    // });
 
-    const newUser = await prisma.usuario.create({
-      data: {
-        username: data.username,
-        email: data.email,
-        password: hashPassw,
-        establesimiento: data.establesimiento,
-      },
-    });
+    // const { password, ...user } = newUser;
 
-    const { password, ...user } = newUser;
-
-    return NextResponse.json(user);
-    console.log(password);
+    // return NextResponse.json(user);
+    // console.log(password);
   } catch (error) {
     return NextResponse.json(
       {
